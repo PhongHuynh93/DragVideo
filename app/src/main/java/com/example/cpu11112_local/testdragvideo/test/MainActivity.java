c183cd76d32b5cdc02e33a48ae4621fbeaff164b
Phong Huynh <phonghdt@vng.com.vn>
2018-10-18 17:57:05 +0700

    change to new youtube layout

--- app/src/main/java/com/example/cpu11112_local/testdragvideo/test/MainActivity.java
+++ app/src/main/java/com/example/cpu11112_local/testdragvideo/test/MainActivity.java
@@ -5,16 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.example.cpu11112_local.testdragvideo.DragVideoView;
+import com.example.cpu11112_local.testdragvideo.DragVideoYoutubeView;
 import com.example.cpu11112_local.testdragvideo.R;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private DragVideoView mDragVideoView;
+    private DragVideoYoutubeView mDragVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         RecyclerView recyclerView = findViewById(R.id.mainRcv);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         recyclerView.setAdapter(new TestDataAdapter(new TestDataAdapter.OnAdapterInteract() {
@@ -24,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }));
 
-        mDragVideoView = (DragVideoView) findViewById(R.id.dragVideo);
-        mDragVideoView.setCallback(new DragVideoView.Callback() {
+        mDragVideoView = (DragVideoYoutubeView) findViewById(R.id.dragVideo);
+        mDragVideoView.setCallback(new DragVideoYoutubeView.Callback() {
             @Override
             public void onDisappear(int direct) {
 
